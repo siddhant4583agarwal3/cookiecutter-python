5757c345a0ca7b7955668443816ddb038eb01615
taolin <tanlin2013@gmail.com>
2023-01-31 14:23:05 +0800

    build: update dev dependencies

--- {{cookiecutter.project_name}}/scripts/lint.sh
+++ {{cookiecutter.project_name}}/scripts/lint.sh
@@ -2,9 +2,13 @@
 set -euxo pipefail
 
 poetry run cruft check
-poetry run mypy --ignore-missing-imports {{cookiecutter.project_name}}/
+poetry run safety check -i 39462 -i 40291
+poetry run bandit -r {{cookiecutter.project_name}}/
 poetry run isort --check --diff {{cookiecutter.project_name}}/ tests/
 poetry run black --check {{cookiecutter.project_name}}/ tests/
 poetry run flake8 {{cookiecutter.project_name}}/ tests/
-poetry run safety check -i 39462 -i 40291
-poetry run bandit -r {{cookiecutter.project_name}}/
+poetry run mypy \
+           --install-types \
+           --non-interactive \
+           {{cookiecutter.project_name}}/
+#  https://mypy.readthedocs.io/en/stable/running_mypy.html#library-stubs-not-installed
