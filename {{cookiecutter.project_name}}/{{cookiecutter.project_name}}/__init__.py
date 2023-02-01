5877b6cfca106007dab6c2db71a13732b3df70d6
taolin <tanlin2013@gmail.com>
2023-02-01 14:39:02 +0800

    feat: add default logger

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
@@ -8,6 +8,14 @@ __version__ = metadata.version(__name__)
 
 # -- Define logger and the associated formatter and handler ------------------
 
-{{cookiecutter.description}}
-"""
-__version__ = "{{cookiecutter.version}}"
+formatter = logging.Formatter(
+    "%(asctime)s [%(filename)s] %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger("{{cookiecutter.project_name}}")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
