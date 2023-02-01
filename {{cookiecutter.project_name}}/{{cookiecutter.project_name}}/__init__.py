5640b4debb6765e1b93ae63633023b348faec6e1
taolin <tanlin2013@gmail.com>
2023-02-01 14:38:41 +0800

    feat: get version from metadata

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/__init__.py
@@ -1,4 +1,12 @@
-"""**{{cookiecutter.project_name}}**
+import logging
+from importlib import metadata
+
+
+# -- Version -----------------------------------------------------------------
+
+__version__ = metadata.version(__name__)
+
+# -- Define logger and the associated formatter and handler ------------------
 
 {{cookiecutter.description}}
 """
