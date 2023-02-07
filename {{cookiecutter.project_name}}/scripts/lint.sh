f8f154f2e74b8695672846adf56d33f93c97a33d
taolin <tanlin2013@gmail.com>
2023-02-07 15:21:46 +0800

    fix: bandit look for setting in pyproject

--- {{cookiecutter.project_name}}/scripts/lint.sh
+++ {{cookiecutter.project_name}}/scripts/lint.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 poetry run cruft check
 poetry run safety check -i 39462 -i 40291
-poetry run bandit -r {{cookiecutter.project_name}}/
+poetry run bandit -c pyproject.toml -r {{cookiecutter.project_name}}/
 poetry run isort --check --diff {{cookiecutter.project_name}}/ tests/
 poetry run black --check {{cookiecutter.project_name}}/ tests/
 poetry run flake8 {{cookiecutter.project_name}}/ tests/
